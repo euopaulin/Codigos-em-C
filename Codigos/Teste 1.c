f6c308a605a757891f0e112932dc018eedab284d
Paulo Henrique <paulohspfc209@gmail.com>
2024-07-09 14:41:34 -0700

    Update

---
+++ Codigos/Teste 1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    int idade;
+    printf("Olá, digite sua idade: ");
+    scanf("%d", &idade);
+    printf("Sua idade eh: %12d", idade);
+    
+return 0;
+}
+
